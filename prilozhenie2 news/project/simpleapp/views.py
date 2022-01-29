415f0ad87b3cb4c16a74a5d1f9327d698a53a977
alibekov55 <93965749+alibekov55@users.noreply.github.com>
2022-01-29 14:14:32 +0600

    news portal
    text

---
+++ prilozhenie2 news/project/simpleapp/views.py
@@ -0,0 +1,22 @@
+from django.views.generic import ListView, DetailView
+from .models import Product, Category
+from datetime import datetime
+
+
+class ProductsList(ListView):
+    model = Product  # указываем модель, объекты которой мы будем выводить
+    template_name = 'products.html'  # указываем имя шаблона, где будет лежать HTML, в котором будут все инструкции о том, как именно пользователю должны вывестись наши объекты
+    context_object_name = 'products'  # это имя списка, в котором будут лежать все объекты, его надо указать, чтобы обратиться к самому списку объектов через HTML-шаблон
+    queryset = Product.objects.order_by('-id')  # настроили пораметр отображения более новый продукт
+
+    # метод get_context_data нужен нам для того, чтобы мы могли передать переменные в шаблон. В возвращаемом словаре context будут храниться все переменные. Ключи этого словаря и есть переменные, к которым мы сможем потом обратиться через шаблон
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()  # добавим переменную текущей даты time_now
+        context['value1'] = None  # добавим ещё одну пустую переменную, чтобы на её примере посмотреть работу другого фильтра
+        return context
+
+class ProductDetail(DetailView):
+    model = Product
+    template_name = 'product.html'
+    context_object_name = 'product'
